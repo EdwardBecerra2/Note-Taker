202ca65a13db75118be8587b78434e91a410fbb3
EdwardBecerra2 <146788503+EdwardBecerra2@users.noreply.github.com>
2024-04-01 18:12:10 -0700

    fixed bugs

--- utils/filesystem.js
+++ utils/filesystem.js
@@ -1,8 +1,8 @@
 const fs = require('fs');
 const util = require('util');
 
-const readFile = util.promisify(fs.readFile);
+const readFromFile = util.promisify(fs.readFile);
 const writeFile = util.promisify(fs.writeFile);
-const appendFile = util.promisify(fs.appendFile);
+const readAndAppend = util.promisify(fs.appendFile);
 
-module.exports = {readFile, writeFile, appendFile};
+module.exports = {readFromFile, writeFile, readAndAppend};
